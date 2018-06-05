4ace06215c5948c29d1ac9197ff6d40666015325
Hsin-En Kung <15233666+Charlie9077@users.noreply.github.com>
2018-06-06 02:17:50 +0800

    bson cleanup

--- model/contract.go
+++ model/contract.go
@@ -2,8 +2,8 @@ package model
 
 type Contract struct {
 	//Specification
-	ImageAMD64       string `json:"imageAMD64" bson:"imageAMD64,omitempty"`
-	RequiredMBMemory int    `json:"requiredMBMemory" bson:"requiredMBMemory,omitempty"`
-	RequiredCPUCores int    `json:"requiredCPUCores" bson:"requiredCPUCores,omitempty"`
-	ServiceName      string `json:"serviceName" bson:"serviceName,omitempty"`
+	ImageAMD64       string `json:"imageAMD64"`
+	RequiredMBMemory int    `json:"requiredMBMemory"`
+	RequiredCPUCores int    `json:"requiredCPUCores"`
+	ServiceName      string `json:"serviceName"`
 }
